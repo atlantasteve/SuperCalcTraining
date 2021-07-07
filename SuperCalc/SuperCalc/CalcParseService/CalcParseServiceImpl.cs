bead8bbd16b216431edd765c1307b3c5b4c856f3
Steven Stewart <sstewart@paygoelectric.com>
2021-07-07 10:50:27 -0400

    Stubbing the Interfaces

---
+++ SuperCalc/SuperCalc/CalcParseService/CalcParseServiceImpl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperCalc
+{
+    public class CalcParseServiceImpl : ICalcParseService
+    {
+        private readonly ICalculatorService _calc;
+        public CalcParseServiceImpl(ICalculatorService calc)
+        {
+            _calc = calc;
+        }
+
+        public string ParseAndCalculate(string input)
+        {
+            return null;
+        }
+
+        public decimal Operand1 { get; private set; }
+        public decimal Operand2 { get; private set; }
+        public string Operator { get; private set; }
+
+        public string InputData { get; private set; }
+
+        public string Output { get; private set; }
+
+    }
+}
---
+++ SuperCalc/SuperCalc/CalculatorService/CalculatorServiceImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperCalc
+{
+    public class CalculatorServiceImpl : ICalculatorService
+    {
+        public CalculatorServiceImpl()
+        {
+
+        }
+
+        public List<string> Operators { get; } = new List<string>() { "+", "-", "*", "x", "/", "^", "%" };
+
+        public decimal Calculate(decimal operand1, string op, decimal operand2)
+        {
+            return -1;
+        }
+    }
+}
