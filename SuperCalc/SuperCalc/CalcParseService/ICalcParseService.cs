21d983ff5f5731b2c4d097f2a9e41ee386ace8f4
Steven Stewart <sstewart@paygoelectric.com>
2021-07-07 10:31:35 -0400

    The Interfaces are created

---
+++ SuperCalc/SuperCalc/CalcParseService/ICalcParseService.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperCalc
+{
+    interface ICalcParseService
+    {
+        string InputData { get; }
+        decimal Operand1 { get; }
+        decimal Operand2 { get; }
+        string Operator { get; }
+        string Output { get; }
+
+        string ParseAndCalculate(string input);
+    }
+}
---
+++ SuperCalc/SuperCalc/CalculatorService/ICalculatorService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SuperCalc
+{
+    public interface ICalculatorService
+    {   
+        List<string> Operators { get; }
+
+        decimal Calculate(decimal operand1, string op, decimal operand2);
+    }
+}
